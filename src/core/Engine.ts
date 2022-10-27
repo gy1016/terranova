133344be06995de441f6ff4579b125f07bcecb4d
lamb <906276457@qq.com>
2022-10-27 17:05:29 +0800

    feat: 创建引擎模块，引入Canvas与Renderer

---
+++ src/core/Engine.ts
@@ -0,0 +1,23 @@
+import { Canvas } from "./Canvas";
+import { Renderer, WebGLRendererOptions } from "./render/Renderer";
+
+export class Engine {
+  protected _canvas: Canvas;
+  protected _renderer: Renderer;
+
+  /**
+   * The canvas to use for rendering.
+   */
+  get canvas(): Canvas {
+    return this._canvas;
+  }
+
+  constructor(canvas: string, webGLRendererOptions?: WebGLRendererOptions) {
+    const webCanvas = new Canvas(<HTMLCanvasElement>document.getElementById(canvas));
+    const hardwareRenderer = new Renderer(webGLRendererOptions);
+    hardwareRenderer.init(webCanvas);
+
+    this._canvas = webCanvas;
+    this._renderer = hardwareRenderer;
+  }
+}
