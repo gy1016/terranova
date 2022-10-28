cdc8878d7326e8f54ef274347cd522e500ea22c8
lamb <906276457@qq.com>
2022-10-28 20:23:17 +0800

    chore: 引入rollup进行打包，抛弃tsc

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import glslify from "rollup-plugin-glslify";
+import serve from "rollup-plugin-serve";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const { NODE_ENV } = process.env;
+
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        dir: "dist",
+        format: "cjs",
+        entryFileNames: "[name].cjs.js",
+      },
+      {
+        dir: "dist",
+        format: "esm",
+        entryFileNames: "[name].esm.js",
+      },
+    ],
+    plugins: [
+      resolve(),
+      glslify({
+        include: [/\.glsl$/],
+      }),
+      commonjs(),
+      typescript({ tsconfig: path.resolve(dirname(fileURLToPath(import.meta.url)), "tsconfig.json") }),
+      NODE_ENV === "development"
+        ? serve({
+            open: true,
+            contentBase: ["public", "dist"],
+            port: 9999,
+          })
+        : null,
+    ],
+  },
+];
