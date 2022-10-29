42b5acc4623d45a65b24e0335d031b5a42f72d76
lamb <1838940294@qq.com>
2022-10-29 11:18:36 +0800

    feat: 新增shader中的一些枚举

---
+++ src/core/shader/enums/ShaderDataGroup.ts
@@ -0,0 +1,15 @@
+/**
+ * Shader data grouping.
+ */
+export enum ShaderDataGroup {
+  /** Scene group. */
+  Scene,
+  /** Camera group. */
+  Camera,
+  /** Renderer group. */
+  Renderer,
+  /** Material group. */
+  Material,
+  /** Geographic group. */
+  Geographic,
+}
