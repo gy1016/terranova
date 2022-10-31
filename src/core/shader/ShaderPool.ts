6e8ba22627539a930a5a7a044e2bddfd8c353e0d
lamb <1838940294@qq.com>
2022-10-31 20:26:34 +0800

    test: 增加测试顶点和片元shader代码

--- src/core/shader/ShaderPool.ts
+++ src/core/shader/ShaderPool.ts
@@ -1,5 +1,7 @@
 import tileVs from "../shaderlib/geographic/tile.vs.glsl";
 import tileFs from "../shaderlib/geographic/tile.fs.glsl";
+import testVs from "../shaderlib/geographic/test.vs.glsl";
+import testFs from "../shaderlib/geographic/test.fs.glsl";
 import { Shader } from "./Shader";
 
 /**
@@ -9,5 +11,6 @@ import { Shader } from "./Shader";
 export class ShaderPool {
   static init(): void {
     Shader.create("tile", tileVs, tileFs);
+    Shader.create("test", testVs, testFs);
   }
 }
---
+++ src/core/shaderlib/geographic/test.vs.glsl
@@ -0,0 +1,5 @@
+attribute vec4 POSITION;
+
+void main() {
+  gl_Position = POSITION;
+}
