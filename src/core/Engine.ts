37a7a12389cd7bae3081ef37c9a564a17d006fc1
lamb <1838940294@qq.com>
2022-11-03 10:34:18 +0800

    feat: 增加引擎设置模块，添加初始化相机位置等配置

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -1,4 +1,7 @@
+import { Vector3 } from "../math";
+import { Camera } from "./Camera";
 import { Canvas } from "./Canvas";
+import { EngineSettings } from "./EngineSettings";
 import { PrimitiveMesh } from "./mesh";
 import { Renderer, WebGLRendererOptions } from "./render/Renderer";
 import { Shader, ShaderPool, ShaderProgramPool } from "./shader";
@@ -8,9 +11,10 @@ import { Texture2D, Texture2DArray, TextureCube } from "./texture";
 ShaderPool.init();
 
 export class Engine {
+  private _settings: EngineSettings;
   protected _canvas: Canvas;
-
   _renderer: Renderer;
+
   /* @internal */
   _magentaTexture2D: Texture2D;
   /* @internal */
@@ -27,10 +31,21 @@ export class Engine {
     return this._canvas;
   }
 
-  constructor(canvas: string | HTMLCanvasElement, webGLRendererOptions?: WebGLRendererOptions) {
+  get setting() {
+    return this._settings;
+  }
+
+  constructor(
+    canvas: string | HTMLCanvasElement,
+    engineSettings?: EngineSettings,
+    webGLRendererOptions?: WebGLRendererOptions
+  ) {
     const webCanvas = new Canvas(
       <HTMLCanvasElement>(typeof canvas === "string" ? document.getElementById(canvas) : canvas)
     );
+
+    this._settings = engineSettings;
+
     const hardwareRenderer = new Renderer(webGLRendererOptions);
     hardwareRenderer.init(webCanvas);
 
@@ -62,10 +77,20 @@ export class Engine {
   run(): void {
     this._canvas.resizeByClientSize();
     this._renderer.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+
+    const camera = new Camera(this);
+    camera.transform.position = new Vector3(10, 10, 10);
+    camera.transform.lookAt(new Vector3(0, 0, 0));
+
+    camera.render();
+
     const cube = PrimitiveMesh.createCuboid(this);
     const testProgram = Shader.find("test")._getShaderProgram(this, new ShaderMacroCollection());
     testProgram.bind();
+    testProgram.uploadAll(testProgram.cameraUniformBlock, camera.shaderData);
+
     cube._draw(testProgram, cube.subMesh);
+
     console.log("run");
   }
 }
---
+++ src/core/EngineSettings.ts
@@ -0,0 +1,8 @@
+import { Vector3 } from "..";
+
+/**
+ * Render settings.
+ */
+export interface EngineSettings {
+  cameraPos?: Vector3;
+}
