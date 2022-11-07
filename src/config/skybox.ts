b2c3176e1441360092848c4d25699d4116b10e56
lamb <1838940294@qq.com>
2022-11-07 11:06:33 +0800

    chore: 增加天空和图片URL的配置文件

---
+++ src/config/skybox.ts
@@ -0,0 +1,35 @@
+export enum CubeMap {
+  TEXTURE_CUBE_MAP_POSITIVE_X = 34069,
+  TEXTURE_CUBE_MAP_NEGATIVE_X,
+  TEXTURE_CUBE_MAP_POSITIVE_Y,
+  TEXTURE_CUBE_MAP_NEGATIVE_Y,
+  TEXTURE_CUBE_MAP_POSITIVE_Z,
+  TEXTURE_CUBE_MAP_NEGATIVE_Z,
+}
+
+export const skyboxConfig = [
+  {
+    target: CubeMap.TEXTURE_CUBE_MAP_POSITIVE_X,
+    url: "http://121.199.160.202/images/skybox/tycho2t3_80_mx.jpg",
+  },
+  {
+    target: CubeMap.TEXTURE_CUBE_MAP_NEGATIVE_X,
+    url: "http://121.199.160.202/images/skybox/tycho2t3_80_px.jpg",
+  },
+  {
+    target: CubeMap.TEXTURE_CUBE_MAP_POSITIVE_Y,
+    url: "http://121.199.160.202/images/skybox/tycho2t3_80_py.jpg",
+  },
+  {
+    target: CubeMap.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+    url: "http://121.199.160.202/images/skybox/tycho2t3_80_my.jpg",
+  },
+  {
+    target: CubeMap.TEXTURE_CUBE_MAP_POSITIVE_Z,
+    url: "http://121.199.160.202/images/skybox/tycho2t3_80_mz.jpg",
+  },
+  {
+    target: CubeMap.TEXTURE_CUBE_MAP_NEGATIVE_Z,
+    url: "http://121.199.160.202/images/skybox/tycho2t3_80_pz.jpg",
+  },
+];
