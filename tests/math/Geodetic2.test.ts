ac514bf7309b7e30424809dd34e5d65fcd98611a
lamb <1838940294@qq.com>
2022-11-11 16:41:07 +0800

    test: 完成地理坐标的测试文件

---
+++ tests/math/Geodetic2.test.ts
@@ -0,0 +1,28 @@
+import { Geodetic2 } from "../../src/math/Geodetic2";
+
+describe("Geodetic2 test", () => {
+  it("geodetic2 constructor", () => {
+    const g1 = new Geodetic2(0, 0);
+    expect(g1.longitude).toEqual(0);
+    expect(g1.latitude).toEqual(0);
+    const g2 = new Geodetic2(90, 90);
+    expect(g2.radLon).toEqual(Math.PI / 2);
+    expect(g2.radLat).toEqual(Math.PI / 2);
+  });
+
+  it("geodetic2 clone and copy", () => {
+    const g3 = new Geodetic2(45, 45);
+    const g4 = g3.clone();
+    expect(g4.longitude).toBe(45);
+    expect(g4.latitude).toBe(45);
+    const g5 = g3.copyFrom(g3);
+    expect(g5.longitude).toBe(45);
+    expect(g5.latitude).toBe(45);
+  });
+
+  it("geodetic2 initial value error", () => {
+    const g6 = new Geodetic2(181, 91);
+    expect(g6.longitude).toBeUndefined();
+    expect(g6.latitude).toBeUndefined();
+  });
+});
---
+++ tests/math/Geodetic3.test.ts
@@ -0,0 +1,32 @@
+import { Geodetic2 } from "../../src/math/Geodetic2";
+import { Geodetic3 } from "../../src/math/Geodetic3";
+
+describe("Geodetic3 test", () => {
+  it("geodetic3 constructor", () => {
+    const g1 = new Geodetic3(0, 0);
+    expect(g1.longitude).toEqual(0);
+    expect(g1.latitude).toEqual(0);
+    expect(g1.height).toEqual(0);
+
+    const g2 = new Geodetic2(90, 90);
+    const g3 = new Geodetic3(g2);
+    expect(g3.radLon).toEqual(Math.PI / 2);
+    expect(g3.radLat).toEqual(Math.PI / 2);
+    expect(g3.height).toEqual(0);
+
+    const g4 = new Geodetic3(0, 0, 0);
+    expect(g4.longitude).toEqual(0);
+    expect(g4.latitude).toEqual(0);
+    expect(g4.height).toEqual(0);
+  });
+
+  it("geodetic3 clone and copy", () => {
+    const g3 = new Geodetic3(45, 45);
+    const g4 = g3.clone();
+    expect(g4.longitude).toBe(45);
+    expect(g4.latitude).toBe(45);
+    const g5 = g3.copyFrom(g3);
+    expect(g5.longitude).toBe(45);
+    expect(g5.latitude).toBe(45);
+  });
+});
