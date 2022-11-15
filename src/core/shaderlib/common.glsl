772283bdb95ddc07fc2dd1fee62e83818638c526
lamb <1838940294@qq.com>
2022-11-15 11:44:16 +0800

    feat: 增加公共的shader

---
+++ src/core/shaderlib/common.glsl
@@ -0,0 +1,6 @@
+#define PI 3.14159265359;
+#define EPSILON 1e-6;
+#define ONEOVERTWOPI 0.15915494309;
+#define ONEOVERPI 0.31830988618;
+
+mat4 geoCoordTranMat = mat4(0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1);
