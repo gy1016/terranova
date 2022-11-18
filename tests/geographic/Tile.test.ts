5448fff53d7e9b32cd391bf67b8b793e9b7a167b
lamb <1838940294@qq.com>
2022-11-18 11:33:40 +0800

    test: 测试瓦片获取临近瓦片行列号的静态方法

--- tests/geographic/Tile.test.ts
+++ tests/geographic/Tile.test.ts
@@ -8,4 +8,10 @@ describe("Tile test", () => {
     expect(extend.max.x).toBe(0);
     expect(extend.max.y).toBe(Math.PI * 6378137.0);
   });
+
+  it("getTileGridByBrother test", () => {
+    const { col, row } = Tile.getTileGridByBrother(1, 0, 0, "left");
+    expect(col).toBe(1);
+    expect(row).toBe(0);
+  });
 });
