d3937b7f9e6cbf8a0e9bc7042fd6e6c3da4ead4a
lamb <1838940294@qq.com>
2022-11-22 10:46:39 +0800

    chore: 光锥球体加入对数深度缓冲

--- src/core/shaderlib/geographic/rayCastedGlobe.vs.glsl
+++ src/core/shaderlib/geographic/rayCastedGlobe.vs.glsl
@@ -1,9 +1,11 @@
+#include <log_depth_vert>
+
 attribute vec4 POSITION;
 uniform mat4 u_MvpMat;
 varying vec3 v_WorldPosition;
 
 void main()
 {
-    gl_Position = u_MvpMat * POSITION;
+    gl_Position = modelToClipCoordinates(POSITION, u_MvpMat, true, 1.0, u_Far);
     v_WorldPosition = POSITION.xyz;
 }
