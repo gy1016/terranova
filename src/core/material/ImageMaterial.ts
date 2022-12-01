804cf78e9318940dbeb421502eb24f5c94f56686
lamb <1838940294@qq.com>
2022-12-01 21:42:39 +0800

    refactor: 添加必须选择一个属性的TS类型

--- src/core/material/ImageMaterial.ts
+++ src/core/material/ImageMaterial.ts
@@ -5,11 +5,19 @@ import { Texture2D } from "../texture";
 import { TextureFormat } from "../texture/enums/TextureFormat";
 import { Material } from "./Material";
 
-interface ImageMaterialOptions {
-  url: string;
+// TODO: 导出到全局
+type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>;
+  }[Keys];
+
+interface Options {
+  url?: string;
   flipY: boolean;
-  base64?: boolean;
+  base64?: string;
 }
+
+type ImageMaterialOptions = RequireOnlyOne<Options, "base64" | "url">;
 
 export class ImageMaterial extends Material {
   /** The texture used by the image material. */
@@ -20,7 +28,7 @@ export class ImageMaterial extends Material {
   constructor(engine: Engine, shader: Shader, options: ImageMaterialOptions) {
     super(engine, shader);
 
-    if (options.base64) {
+    if (options.base64 != undefined) {
       const image = new Image();
       image.src = "data:image/png;base64," + options.base64;
       this._initialTexture(image, options);
