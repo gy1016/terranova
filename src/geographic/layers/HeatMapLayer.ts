09a02d60188749e1ea7089ba00eb47883bb0a81f
lamb <1838940294@qq.com>
2022-12-02 17:02:00 +0800

    refactor: 缩放热力图层进行对应更新

--- src/geographic/layers/HeatMapLayer.ts
+++ src/geographic/layers/HeatMapLayer.ts
@@ -31,6 +31,7 @@ export class HeatMapLayer extends Layer {
   gradient: number[][] | string[];
   maxIntensity: number;
   tiles: Map<string, Tile>;
+  _lastZoom: number = -1;
 
   /**
    * Convert the incoming color band to an array of floating point types.
@@ -129,15 +130,14 @@ export class HeatMapLayer extends Layer {
     this.send("setGradient", postGradient);
   }
 
-  addPoints(points: HeatPoint[]) {
+  // 根据所有热力点位生成对应层级的瓦片，会清空当前tiles的map容器
+  _accordPointsGenerateTiles(points: HeatPoint[], zoom: number): number[][] {
     const postPoints: number[][] = [[points[0].lat, points[0].lng, points[0].weight]];
-    this.points = this.points.concat(points);
     // 求出所有点位的包围盒
     const minGeodetic2 = new Geodetic2(points[0].lng, points[0].lat);
     const maxGeodetic2 = new Geodetic2(points[0].lng, points[0].lat);
-
-    for (let i = 1; i < this.points.length; ++i) {
-      const point = this.points[i];
+    for (let i = 1; i < points.length; ++i) {
+      const point = points[i];
       minGeodetic2.latitude = Math.min(minGeodetic2.latitude, point.lat);
       minGeodetic2.longitude = Math.min(minGeodetic2.longitude, point.lng);
       maxGeodetic2.latitude = Math.max(maxGeodetic2.latitude, point.lat);
@@ -148,18 +148,32 @@ export class HeatMapLayer extends Layer {
     const minMercator = minGeodetic2.toMercator();
     const maxMercator = maxGeodetic2.toMercator();
 
-    const minTileCoord = Tile.getTileRowAndCol(minMercator.x, minMercator.y, this.engine.scene.camera.level);
-    const maxTileCoord = Tile.getTileRowAndCol(maxMercator.x, maxMercator.y, this.engine.scene.camera.level);
+    const minTileCoord = Tile.getTileRowAndCol(minMercator.x, minMercator.y, zoom);
+    const maxTileCoord = Tile.getTileRowAndCol(maxMercator.x, maxMercator.y, zoom);
 
+    const minRow = Math.min(minTileCoord.row, maxTileCoord.row);
+    const minCol = Math.min(minTileCoord.col, maxTileCoord.col);
+
+    const maxRow = Math.max(minTileCoord.row, maxTileCoord.row);
+    const maxCol = Math.max(minTileCoord.col, maxTileCoord.col);
+
+    // ! 这里应该会被GC吧，这里欠缺考虑
     this.tiles = new Map();
-    for (let row = minTileCoord.row; row <= maxTileCoord.row; ++row) {
-      for (let col = minTileCoord.col; col <= maxTileCoord.col; ++col) {
+
+    // TODO: 其实有些瓦片可能确实不需要，按理说只需要计算出点位对应的瓦片就行，待优化
+    for (let row = minRow; row <= maxRow; ++row) {
+      for (let col = minCol; col <= maxCol; ++col) {
         const tile = new Tile(this.engine, this.engine.scene.camera.level, row, col);
         this.tiles.set(tile.key, tile);
       }
     }
 
-    this.send("addPoints", postPoints);
+    return postPoints;
+  }
+
+  addPoints(points: HeatPoint[]) {
+    this.points = this.points.concat(points);
+    this.send("addPoints", this._accordPointsGenerateTiles(this.points, this.engine.scene.camera.level));
   }
 
   createTile(x: number, y: number) {
@@ -173,23 +187,33 @@ export class HeatMapLayer extends Layer {
 
   gradientSeted(length: number) {
     Logger.info(`Gradient updated with ${length} color steps.`);
+    this.updateTiles();
   }
 
   zoomSeted(zoom: number) {
     Logger.info(`The zoom in WASM is successfully set to ${zoom}.`);
+    // zoom变了需要重新计算一下需要生成的瓦片
+    this._accordPointsGenerateTiles(this.points, zoom);
+    // 然后通知更新瓦片
+    this.updateTiles();
   }
 
   maxIntensitySeted(maxIntensity: number) {
     Logger.info(`The maximum density is set to ${maxIntensity}.`);
+    this.updateTiles();
   }
 
   radiusSeted(radius: number) {
     Logger.info(`The influence radius of the hot spot is set to ${radius}`);
+    this.updateTiles();
   }
 
+  // 瓦片创建之后实例化材质
   tileCreated(tileInfo: TileCoord & { base64: string }) {
     Logger.info(`I will create the tile row ${tileInfo.row}, col: ${tileInfo.col} and level: ${tileInfo.level}`);
+    // 有可能此时tiles已经更换了层级，所以当tile不存在时直接return
     const tile = this.tiles.get(Tile.generateKey(tileInfo.level, tileInfo.row, tileInfo.col));
+    if (!tile) return;
     tile.material = new ImageMaterial(this.engine, Shader.find("tile"), { flipY: true, base64: tileInfo.base64 });
   }
 
@@ -218,14 +242,20 @@ export class HeatMapLayer extends Layer {
     }
   }
 
-  // TODO: 抽象一个图层类，提取公共方法
+  // 热力图瓦片我们全部渲染，可能也没有几张
   _render(level: number) {
+    if (this._lastZoom != -1 && this._lastZoom != level) {
+      this.setZoom(level);
+    }
+
+    this._lastZoom = level;
+
     const engine = this.engine;
     const camera = engine.scene.camera;
 
     for (const tile of this.tiles.values()) {
       const { mesh, material } = tile;
-      // ! 按道理说这里material肯定是有的，但是会报错，先加个判断
+      // ! 可能子线程还没有发过来base64
       if (!material) continue;
       material.shaderData.setTexture(ImageMaterial._sampleprop, (material as ImageMaterial).texture2d);
       const program = material.shader._getShaderProgram(engine, Shader._compileMacros);
