4ae619e6317af7ccf97e2c51a93d5248a3bc7d86
lamb <1838940294@qq.com>
2023-03-13 21:01:19 +0800

    fix: 修改变量导出未生效

--- src/core/shaderlib/common.glsl
+++ src/core/shaderlib/common.glsl
@@ -1,6 +1,7 @@
 #define PI 3.14159265359;
 #define EPSILON 1e-6;
-#define ONEOVERTWOPI 0.15915494309;
-#define ONEOVERPI 0.31830988618;
+
+const float oneOverTwoPi = 0.15915494309189535;
+const float oneOverPi = 0.3183098861837907;
 
 mat4 geoCoordTranMat = mat4(0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1);
